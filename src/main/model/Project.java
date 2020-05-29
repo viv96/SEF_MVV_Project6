f4512b039bc69b0c45d51ed7cc5c2c7b6ce9f0e0
Maxime BOURMAUD <maxime.bourmaud@epitech.eu>
2020-05-29 12:32:06 +1000

    Feat: Rebase Vijit on our work

--- src/main/model/Project.java
+++ src/main/model/Project.java
@@ -1,89 +1,273 @@
 package model;
 
-import enumerations.availability;
-
-import java.util.ArrayList;
+import java.util.*;
+import java.time.LocalDate;
 import java.io.Serializable;
 
 public class Project implements Serializable {
+    private static int projectNumID = 0;
     private String projectID;
     private String projectName;
-    private String projectStatus;
-    private String projectStartDate;
-    private String projectEndDate;
-    private int projectDaysPerWeek;
-    private ArrayList<Activity> activities = new ArrayList<Activity>();
+    private String projectDescription;
+    private LocalDate projectStartDate;
+    private LocalDate projectEndDate;
+    private pstatus projectStatus;
+    private enum pstatus {OPEN, ASSIGNED, COMPLETED}; //Still need to think about the types of status we need
+    private ArrayList<Activity> listOfActivities = new ArrayList<Activity>();
 
-    public Project(String projectID, String projectName, String projectStatus, ArrayList<Activity> activities, String projectStartDate, String projectEndDate, int projectDaysPerWeek) {
-        super();
-        this.projectID = projectID;
-        this.projectName = projectName;
-        this.projectStatus = projectStatus;
-        this.activities = activities;
-        this.projectStartDate = projectStartDate;
-        this.projectEndDate = projectEndDate;
-        this.projectDaysPerWeek = projectDaysPerWeek;
+    //Getter method List of Activities
+    public ArrayList<Activity> getListOfActivities() {
+        return listOfActivities;
     }
 
-    public int getProjectDaysPerWeek() {
-        return projectDaysPerWeek;
+    //Constructor for initializing the Project
+    public Project(String name, String description, LocalDate startDate, LocalDate endDate){
+        this.projectName = name;
+        this.projectDescription = description;
+        this.projectStartDate = startDate;
+        this.projectEndDate = endDate;
+        this.projectStatus = pstatus.OPEN;
+        projectNumID++;
+        generateProjectId();
     }
 
-    public void setProjectDaysPerWeek(int projectDaysPerWeek) {
-        this.projectDaysPerWeek = projectDaysPerWeek;
+    /*******************************************************************************************************************
+     * Method name       : generateProjectId()
+     * Return type       : void
+     * Creator           : Vijit Kumar (s3799493)
+     * Method description: This method is called in a constructor and then generates a unique
+     *                     ID for an project object.
+     ******************************************************************************************************************/
+    public void generateProjectId() {
+        String s1 = "P";
+        String str = String.format("%d", projectNumID);
+        String s = s1+str;
+        this.projectID = s;
     }
 
-    public String getProjectStartDate() {
-        return projectStartDate;
+    /*******************************************************************************************************************
+     * Method name       : UpdateListOfActivities(Activity)
+     * Return type       : void
+     * Creator           : Vijit Kumar (s3799493)
+     * Method description: This method adds the activity to the list of activity.
+     ******************************************************************************************************************/
+    public void updateListOfActivities(Activity activity) {
+        this.listOfActivities.add(activity);
     }
 
-    public void setProjectStartDate(String projectStartDate) {
-        this.projectStartDate = projectStartDate;
+    /*******************************************************************************************************************
+     * Method name       : criticalPath()
+     * Return type       : void
+     * Creator           : Vijit Kumar (s3799493)
+     * Method description: This method calculates the critical path of the Project by doing a backward and forward pass
+     *                     which will update the values of early  start, late start, early finish, and late finish of
+     *                     each activity, thus calculating the critical path.
+     ******************************************************************************************************************/
+    public void criticalPath(){
+        //STEP 1: Do the Forward pass
+        for(int index1=0; index1<listOfActivities.size(); index1++){
+            forwardPassInCriticalPath(listOfActivities.get(index1));
+        }
+
+        //STEP 2: Update the lateFinish and lateStart of the Last Activities with largest early finish value
+        double tempLargestEarlyFinish = listOfActivities.get(0).getEarlyFinish();
+        int indexOfLargestEarlyFinishActivity = 0;
+        for(int index2=0; index2<listOfActivities.size(); index2++){
+            if(tempLargestEarlyFinish < listOfActivities.get(index2).getEarlyFinish()){
+                tempLargestEarlyFinish = listOfActivities.get(index2).getEarlyFinish();
+                indexOfLargestEarlyFinishActivity = index2;
+            }
+        }
+        //Getting the list of indexes of Last activities
+        ArrayList<Integer> indexOfLastActivities = criticalPathHelper();
+        for(int index3=0; index3<indexOfLastActivities.size(); index3++){
+            double tempEstimatedTime = listOfActivities.get(indexOfLastActivities.get(index3)).getEstimatedTimeInWeek();
+            listOfActivities.get(indexOfLastActivities.get(index3)).setLateFinish(tempLargestEarlyFinish);
+            listOfActivities.get(indexOfLastActivities.get(index3)).setLateStart(tempLargestEarlyFinish-tempEstimatedTime);
+
+            //Updating total slack value
+            double tempLateFinish = listOfActivities.get(indexOfLastActivities.get(index3)).getLateFinish();
+            double tempEarlyFinish = listOfActivities.get(indexOfLastActivities.get(index3)).getEarlyFinish();
+            listOfActivities.get(indexOfLastActivities.get(index3)).setTotalSlack(tempLateFinish-tempEarlyFinish);
+        }
+
+        //STEP 3: Do the Backward Pass
+        backwardPassInCriticalPath(indexOfLastActivities);
     }
 
-    public String getProjectEndDate() {
-        return projectEndDate;
+    /*******************************************************************************************************************
+     * Method name       : forwardPassInCriticalPath(Activity )
+     * Return type       : void
+     * Creator           : Vijit Kumar (s3799493)
+     * Method description: This method is recursively called to calculate the value of early start and early finish
+     ******************************************************************************************************************/
+    private void forwardPassInCriticalPath(Activity activity){
+        //This is a terminating condition for recursion
+        if(activity.getEarlyFinish() != -1 ){
+            return;
+        }
+
+        //This checks if there are multiple dependencies and then call recursively
+        for(int i=0; i<activity.getDependencies().size(); i++){
+            forwardPassInCriticalPath(activity.getDependencies().get(i));
+        }
+
+        //This loop checks if there are multiple dependencies then we choose the maximum value of dependency's early finish
+        double tempDependencyEarlyFinish = activity.getDependencies().get(0).getEarlyFinish();
+        for(int j=0; j<activity.getDependencies().size(); j++){
+            if(tempDependencyEarlyFinish < activity.getDependencies().get(j).getEarlyFinish()){
+                tempDependencyEarlyFinish = activity.getDependencies().get(j).getEarlyFinish();
+            }
+        }
+
+        //Updating the value of earlyStart and earlyFinish
+        activity.setEarlyStart(tempDependencyEarlyFinish);
+        activity.setEarlyFinish(activity.getEarlyStart()+activity.getEstimatedTimeInWeek());
     }
 
-    public void setProjectEndDate(String projectEndDate) {
-        this.projectEndDate = projectEndDate;
+    /*******************************************************************************************************************
+     * Method name       : backwardPassInCriticalPath(Activity )
+     * Return type       : void
+     * Creator           : Vijit Kumar (s3799493)
+     * Method description: This method uses Breadth First Search Method to do the backward pass. In this method all
+     *                     the values of lateStart, lateFinish, and total slack are calculated.
+     ******************************************************************************************************************/
+    private void backwardPassInCriticalPath(ArrayList<Integer> indexOfActivities){
+        //Create a Queue that will keep the information of indexes
+        Queue<Integer> indexQueue = new LinkedList<>();
+
+        //Updating the queue
+        for(int index1=0; index1<indexOfActivities.size(); index1++){
+            indexQueue.add(indexOfActivities.get(index1));
+        }
+
+        //This is where the BFS is happening aka MAGIC :)
+        while(indexQueue.peek() != null){
+            if(listOfActivities.get(indexQueue.peek()).getDependencies() != null) {
+                int sizeOfActivityDependency = listOfActivities.get(indexQueue.peek()).getDependencies().size();
+
+                for (int index2 = 0; index2 < sizeOfActivityDependency; index2++) {
+                    double tempLateStartOfHead = listOfActivities.get(indexQueue.peek()).getLateStart();
+                    String tempDependencyActivityName =
+                            listOfActivities.get(indexQueue.peek())
+                                    .getDependencies()
+                                    .get(index2)
+                                    .getActivityName();
+
+                    //Adding the dependency index to queue
+                    for (int index3 = 0; index3 < listOfActivities.size(); index3++) {
+                        if (listOfActivities.get(index3).getActivityName().equalsIgnoreCase(tempDependencyActivityName)) {
+                            indexQueue.add(index3);
+                            break;
+                        }
+                    }
+
+                    //Starting here Updating the values of Late Finish and Late Start of Dependencies
+                    if (listOfActivities.get(indexQueue.peek()).getDependencies().get(index2).getLateFinish() == -1) {
+                        //Updating the value of Late Finish of Dependency
+                        listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .setLateFinish(tempLateStartOfHead);
+
+                        //Updating the value of Late Start of Dependency
+                        double tempLateFinishOfDependency =
+                                listOfActivities.get(indexQueue.peek())
+                                        .getDependencies()
+                                        .get(index2)
+                                        .getLateFinish();
+                        double tempDurationOfDependency =
+                                listOfActivities.get(indexQueue.peek())
+                                        .getDependencies()
+                                        .get(index2)
+                                        .getEstimatedTimeInWeek();
+                        listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .setLateStart(tempLateFinishOfDependency - tempDurationOfDependency);
+
+                        //This is updating the value of total Slack of dependency
+                        double tempEarlyFinishOfDependency =
+                                listOfActivities.get(indexQueue.peek())
+                                        .getDependencies()
+                                        .get(index2)
+                                        .getEarlyFinish();
+                        listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .setTotalSlack(tempLateFinishOfDependency - tempEarlyFinishOfDependency);
+                    } else if (listOfActivities.get(indexQueue.peek())
+                            .getDependencies()
+                            .get(index2)
+                            .getLateFinish() > listOfActivities.get(indexQueue.peek()).getLateStart()) {
+                        listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .setLateFinish(listOfActivities.get(indexQueue.peek()).getLateStart());
+
+                        //Updating the value of Late Start of Dependency
+                        double tempLateFinishOfDependency = listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .getLateFinish();
+                        double tempDurationOfDependency =
+                                listOfActivities.get(indexQueue.peek())
+                                        .getDependencies()
+                                        .get(index2)
+                                        .getEstimatedTimeInWeek();
+                        listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .setLateStart(tempLateFinishOfDependency - tempDurationOfDependency);
+
+                        //Updating the value of total Slack of dependency
+                        double tempEarlyFinishOfDependency = listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .getEarlyFinish();
+                        listOfActivities.get(indexQueue.peek())
+                                .getDependencies()
+                                .get(index2)
+                                .setTotalSlack(tempLateFinishOfDependency - tempEarlyFinishOfDependency);
+                    }
+                }
+            }
+            //Removing the index from queue after the updates are done
+            indexQueue.poll();
+        }
     }
 
-    public ArrayList<Activity> getActivities() {
-        return activities;
+    /*******************************************************************************************************************
+     * Method name       : criticalPathHelper()
+     * Return type       : ArrayList<Integer>
+     * Creator           : Vijit Kumar (s3799493)
+     * Method description: This method finds the last activities that are not part of any dependencies and return their
+     *                     index position in listOfActivities.
+     ******************************************************************************************************************/
+    private ArrayList<Integer> criticalPathHelper(){
+        //First add all the dependencies in a hash set so that multiple copies are not saved
+        Set<String> dependenciesList = new HashSet<>();
+        for(int index1=0; index1<listOfActivities.size(); index1++){
+            if(listOfActivities.get(index1).getDependencies() != null) {
+                for (int index2 = 0; index2 < listOfActivities.get(index1).getDependencies().size(); index2++) {
+                    dependenciesList.add(listOfActivities.get(index1).getDependencies().get(index2).getActivityName());
+                }
+            }
+        }
+
+        //Now check the if Activity name exists inside the HashSet
+        ArrayList<Integer> indexOfLastActivites = new ArrayList<>();
+        for(int index3=0; index3<listOfActivities.size(); index3++){
+            if(!(dependenciesList.contains(listOfActivities.get(index3).getActivityName()))){
+                indexOfLastActivites.add(index3);
+            }
+        }
+        return indexOfLastActivites;
     }
 
-    public void setActivities(ArrayList<Activity> activities) {
-        this.activities = activities;
-    }
-
-    public String getProjectID() {
-        return projectID;
-    }
-
-    public void setProjectID(String projectID) {
-        this.projectID = projectID;
-    }
-
-    public String getProjectName() {
-        return this.projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getProjectStatus() {
-        return this.projectStatus;
-    }
-
-    public void setProjectStatus(String projectStatus) {
-        this.projectStatus = projectStatus;
-    }
-
+    /*
     @Override
     public String toString() {
         return "model.Project [name = " + this.projectName + ", status = " + this.projectStatus + ", activities = " + this.activities + "]";
-
-    }
+    }*/
 }
